032e327185fea5250d3af2d292167f4313cfa5bb
7elww7adk-blip <7elw.w.7adk@gmail.com>
2026-02-23 19:00:46 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -3,12 +3,11 @@ window.APP_CONFIG = {
     "https://script.google.com/macros/s/AKfycbxWetY0OplMAzjyuAY5EB2v_qdpxtfh4oZUnwLexI_a32AspbF779XrBjJIdwSMN-Yqbg/exec",
   DEFAULT_CATEGORY: "الكهرباء",
   CACHE_TTL_MS: 10 * 60 * 1000,
-  AUTH_HOOK_URL: "",
   FALLBACK_SETTINGS: {
     site_title: "المصدر",
     currency: "جنيه",
     shipping: "0",
-    whatsapp_number: "01025048784",
+    whatsapp_number: "201000000000",
     write_orders: "false"
   }
 };
